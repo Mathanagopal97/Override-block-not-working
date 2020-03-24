b16898312614b4958213dcf1200e2cdca0ea7662
Mathanagopal Sankarasubramanian <mathanagopal@oxsoftwares.com>
2020-03-24 09:33:37 +0530

    Initial Commit

---
+++ AXAStudios/Customer/Block/Account/AuthorizationLink.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace AXAStudios\Customer\Block\Account;
+
+
+class AuthorizationLink extends \Magento\Customer\Block\Account\AuthorizationLink
+{
+  public function FunctionName($value='')
+  {
+    // code...
+    //Write your code here.
+  }
+}
